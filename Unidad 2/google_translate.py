56eec0b1a2a0a5a759455fcc34500817a41d924d
JohanGarciaLino <johangarcia641@gmail.com>
2023-09-07 14:46:24 -0600

    first translate server

--- Unidad 2/google_translate.py
+++ Unidad 2/google_translate.py
@@ -15,7 +15,8 @@ headers = {"content-type": "application/x-www-form-urlencoded", "Accept-Encoding
 
 response = requests.post(url, data=payload, headers=headers)
 
+print(response.json())
+
 texto_traducido = response.json()["data"]["translations"][0]["translatedText"]
 
-print(response.json())
 print(texto_traducido)
